cfe9c2b54f9f201883f169849e07e2f948cbc701
deethar <dieterderksen@googlemail.com>
2020-01-22 20:46:29 +0100

    yes?

--- Swords.py
+++ Swords.py
@@ -6,13 +6,15 @@ win = pg.display.set_mode((1200,800))
 
 pg.display.set_caption("Schlag den Dietron")
 
-walkRight = [pg.image.load("CharackterA_R_1.png"), pg.image.load("CharackterA_R_2.png"), pg.image.load("CharackterA_R_3.png"), pg.image.load("CharackterA_R_4.png"), pg.image.load("CharackterA_R_5.png"), pg.image.load("CharackterA_R_6.png"), pg.image.load("CharackterA_R_7.png"), pg.image.load("CharackterA_R_8.png"), pg.image.load("CharackterA_R_9.png")]
-walkLeft = [pg.image.load("CharackterA_L_1.png"), pg.image.load("CharackterA_L_2.png"), pg.image.load("CharackterA_L_3.png"), pg.image.load("CharackterA_L_4.png"), pg.image.load("CharackterA_L_5.png"), pg.image.load("CharackterA_L_6.png"), pg.image.load("CharackterA_L_7.png"), pg.image.load("CharackterA_L_8.png"), pg.image.load("CharackterA_L_9.png")]
-char = pg.image.load("CharakterA_char.png")
-ducking = pg.image.load("Charakter_A_ducking.png")
+walkRight = [pg.image.load("pics/pics/CharakterA_R_1.png"), pg.image.load("pics/pics/CharakterA_R_2.png"), pg.image.load("pics/pics/CharakterA_R_3.png"), pg.image.load("pics/pics/CharakterA_R_4.png"), pg.image.load("pics/pics/CharakterA_R_5.png"), pg.image.load("pics/pics/CharakterA_R_6.png"), pg.image.load("pics/pics/CharakterA_R_7.png"), pg.image.load("pics/pics/CharakterA_R_8.png"), pg.image.load("pics/pics/CharakterA_R_9.png")]
+walkLeft = [pg.image.load("pics/pics/CharakterA_L_1.png"), pg.image.load("pics/pics/CharakterA_L_2.png"), pg.image.load("pics/pics/CharakterA_L_3.png"), pg.image.load("pics/pics/CharakterA_L_4.png"), pg.image.load("pics/pics/CharakterA_L_5.png"), pg.image.load("pics/pics/CharakterA_L_6.png"), pg.image.load("pics/pics/CharakterA_L_7.png"), pg.image.load("pics/pics/CharakterA_L_8.png"), pg.image.load("pics/pics/CharakterA_L_9.png")]
+char = pg.image.load("pics/CharakterA_char.png")
+ducking = pg.image.load("pics/Charakter_A_ducking.png")
 screenWidth = 1200
 
 clock = pg.time.Clock()
+
+
 #player
 class player(object):
     def __init__(self, x, y, width, height):
